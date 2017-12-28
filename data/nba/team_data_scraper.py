219f864128f9ab86a220bbbf45c679dca6eb6129
Alex Papanicolaou <alex.papanic@gmail.com>
2017-12-27 20:40:22 -0800

    pythag wins

--- data/nba/team_data_scraper.py
+++ data/nba/team_data_scraper.py
@@ -43,6 +43,7 @@ STR_FIELDS = [
     "team_id",
     "avg_ht"
 ]
+OPP_FIELDS_SKIP = ['lg_id', 'team_id', "wins", "losses", "rank_team", "g", "mp"]
 STATS = [
     "lg_id",
     "team_id",
@@ -56,11 +57,9 @@ STATS = [
     "def_rtg",
     "def_rtg_rel",
     "rank_team",
-    "foo",
     "avg_age",
     "avg_ht",
     "avg_wt",
-    "foo",
     "g",
     "mp",
     "fg",
@@ -83,7 +82,28 @@ STATS = [
     "blk",
     "tov",
     "pf",
-    "pts"
+    "pts",
+    "opp_fg",
+    "opp_fga",
+    "opp_fg_pct",
+    "opp_fg3",
+    "opp_fg3a",
+    "opp_fg3_pct",
+    "opp_fg2",
+    "opp_fg2a",
+    "opp_fg2_pct",
+    "opp_ft",
+    "opp_fta",
+    "opp_ft_pct",
+    "opp_orb",
+    "opp_drb",
+    "opp_trb",
+    "opp_ast",
+    "opp_stl",
+    "opp_blk",
+    "opp_tov",
+    "opp_pf",
+    "opp_pts"
 ]
 
 
@@ -138,16 +158,44 @@ def fetch_team_csv(team):
                         stat = float(td.string)
                 season[stat_field] = stat
 
+    resp = requests.get(URL.format(team) + "opp_stats_basic_totals.html")
+    soup = bs4.BeautifulSoup(resp.text, "html5lib")
+    table = soup.table
+
+    for tr in table.tbody.find_all("tr"):
+        if 'class' in tr.attrs:
+            if 'thead' in tr['class']:
+                continue
+        year = tr.th.string
+        year0 = int(year.split("-")[0])
+        if year0 < 1973 or year0 > 2016:
+            continue
+        season = seasons[year]
+        for td in tr.find_all("td"):
+            stat_field = td["data-stat"]
+            # and (stat_field not in OPP_FIELDS_SKIP):
+            # print(stat_field, stat_field in STATS,
+            #       not stat_field in OPP_FIELDS_SKIP)
+            if (stat_field in STATS) and not (stat_field in OPP_FIELDS_SKIP):
+                if stat_field in STR_FIELDS:
+                    stat = str(td.string)
+                else:
+                    if td.string is None:
+                        stat = np.nan
+                    else:
+                        stat = float(td.string)
+                season[stat_field] = stat
+
     df = pd.DataFrame.from_dict(seasons, orient='index').reset_index()
     df['year'] = df['index']
     del df['index']
     df["team"] = team
-    df['games'] = df['wins'] + df['losses']
-    df['win_pct'] = df['wins'] / df['games']
-    df['win_logratio'] = np.log(df['wins'] / df['losses'])
+    # df['games'] = df['wins'] + df['losses']
+    # df['win_pct'] = df['wins'] / df['games']
+    # df['win_logratio'] = np.log(df['wins'] / df['losses'])
 
-    df['net_rtg'] = df['off_rtg'] - df['def_rtg']
-    df['rtg_logratio'] = np.log(df['off_rtg'] / df['def_rtg'])
+    # df['net_rtg'] = df['off_rtg'] - df['def_rtg']
+    # df['rtg_logratio'] = np.log(df['off_rtg'] / df['def_rtg'])
 
     return df
 
